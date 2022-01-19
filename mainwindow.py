f33b3015af4a99e04abcef4acaaa99eb54f3e7a6
Ibrahim Boubakri <ibrahim.boubakri@ensi-uma.tn>
2022-01-19 11:48:11 +0100

    v1.7
    Added console and exception for double scans and not selecting a target

--- mainwindow.py
+++ mainwindow.py
@@ -3,33 +3,40 @@ from PySide6.QtUiTools import QUiLoader
 from PySide6.QtWidgets import QApplication, QPushButton, QListWidgetItem
 from PySide6.QtCore import QFile, QIODevice, Slot
 from NetworkScanner.networkScanner import scanner
-
+targets = set()
+scanning = False
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-
     ui_file_name = "form.ui"
     ui_file = QFile(ui_file_name)
     loader = QUiLoader()
     window = loader.load(ui_file)
     ui_file.close()
-
-    targets = set()
+    window.console.setText("")
 
     @Slot()
-    def scan():
-        print("Scanner begin")
-        listHosts = scanner()
-        window.listWidget.clear()
-        print(listHosts)
-        for host in listHosts:
-            listWidgetItem = QListWidgetItem(host["ip"]+' : '+host["mac"])
-            window.listWidget.addItem(listWidgetItem)
-    
+    def scan(scanning):
+        if(scanning==False) :
+            print("Scanner begin")
+            scanning = True
+            listHosts = scanner()
+            window.listWidget.clear()
+            print(listHosts)
+            for host in listHosts:
+                listWidgetItem = QListWidgetItem(host["ip"]+' : '+host["mac"])
+                window.listWidget.addItem(listWidgetItem)
+            scanning = False
+        else :
+            window.console.append("Already scanning !")    
     @Slot()
     def addTargetClicked() :
-        targets.add(window.listWidget.selectedItems()[0].text())
-        print(window.listWidget.selectedItems()[0].text() + " added to your targets list")
-    
+        try :
+            targets.add(window.listWidget.selectedItems()[0].text())
+            output = window.listWidget.selectedItems()[0].text() + " added to your targets list"
+            print (output)
+            window.console.append(output)
+        except IndexError:
+            window.console.append("You didn't select a target")
 
 
     window.scan.clicked.connect(scan)
